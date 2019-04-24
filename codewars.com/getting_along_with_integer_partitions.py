6a7193a7ac53737d8787d9fb3e572ee37b897530
zhouweixian <laxian2010@qq.com>
2019-04-25 00:09:19 +0800

    codewars :Explosive Sum

--- codewars.com/getting_along_with_integer_partitions.py
+++ codewars.com/getting_along_with_integer_partitions.py
@@ -1,3 +1,11 @@
+from functools import reduce
+import time
+
+
+def shrink(lst):
+    return set(map(lambda l: reduce(lambda x, y: x * y, l), lst))
+
+cache = {}
 def f(n):
     if n == 0:
         return [[]]
@@ -5,24 +13,33 @@ def f(n):
         return [[1]]
     ret = []
     for m in range(n, 0, -1):
-        # r = min(m, n - m)
         r = n - m
-        fr = f(r)
+        if cache.get(r):
+            fr = cache[r]
+        else:
+            fr = f(r)
+            cache[r] = fr
         ret.extend(accept(fr, [m]))
         print(ret)
-    return dummy(ret, n)
+    return ret
+
+
+def accept(lst, n):
+    return [n + x for x in lst if x ==[] or max(x)<=n[0]]
+
 
 def dummy(lst, s):
     for l in lst:
-        while sum(l)<s:
+        while sum(l) < s:
             l.append(1)
     return lst
 
-def accept(lst, n):
-    return [n + x for x in lst]
-
 
 if __name__ == "__main__":
-    print(f(5))
+    tick = time.time()
+    for i in range(1):
+        print(len(f(50)))
+    print(time.time()-tick)
     # print(f(7))
     # print(dummy([[1,1],[1,2]], 3))
+    # print(shrink([[1,2],[2,3]]))
