c0f7820791a577556ec4a1beb95bceab46bae5d0
laxian <zhouweixian@etiantian.com>
2019-04-26 16:58:53 +0800

    codewars: how many numbers III

--- codewars.com/how_many_numbers3.py
+++ codewars.com/how_many_numbers3.py
@@ -1,36 +1,23 @@
 # https://www.codewars.com/kata/how-many-numbers-iii/train/python
-def find_all(sum_dig, digs, limit=None):
-    ret = []
-    limit = limit if limit else 9
-    if sum_dig > digs*limit or sum_dig < digs:
-        return []
+def find_all(sum_dig, digs, minimal=None):
+    lst = [int(x) for x in find_all_list(sum_dig, digs)]
+    return [len(lst), min(lst), max(lst)] if lst else []
+
+def find_all_list(sum_dig, digs, minimal=None):
     if digs == 1:
-        return [sum_dig] if not limit or limit >= sum_dig else []
-    if sum_dig == digs:
-        return [1] * digs
-
-    if sum_dig < 9:
-        lst = [sum_dig-digs+1] + [digs-1]
-    else:
-        lst = [9]*(sum_dig//9) + [sum_dig%9]
-    # print(lst)
-    while lst:
-        curr = lst.pop()
-        print(curr)
-        while lst[-1] > 0:
-            pre = lst[-1] if lst else None
-            sub = find_all(curr, digs - len(lst), pre)
-            if sub:
-                print(lst)
-                print(lst+sub)
-                ret.append(lst+sub)
-                # return lst + sub
-            if pre:
-                lst[-1] -= 1
-                curr = sum_dig - sum(lst)
-        lst.pop()
-    return ret
+        return str(sum_dig) if sum_dig < 10 else None
+    result = []
+    minimal = minimal if minimal else 1
+    for i in range(minimal, sum_dig // digs + 1):
+        s = find_all_list(sum_dig - i, digs - 1, i)
+        if isinstance(s, list):
+            ret = ["%r%s" % (i, c) for c in s]
+            result.extend(ret)
+        elif s:
+            ret = "%r%s" % (i, s)
+            result.append(ret)
+    return result
 
 
-if __name__ == '__main__':
-    print(find_all(10, 3))
+if __name__ == "__main__":
+    print(find_all(84, 4))
